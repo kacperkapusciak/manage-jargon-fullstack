dfafddd6ff061e0a1a29dc7be6ce1cf6bfcffbe6
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-06-23 14:04:58 +0200

    Added Jargon validation

--- src/server/models/jargon.js
+++ src/server/models/jargon.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
-// const data = require('../../../data/jargon');
+const Ajv = require('ajv');
+
+const ajv = new Ajv();
+require('ajv-async')(ajv);
 
 const jargonSchema = {
   name: {
     type: String,
     required: true,
     min: 2,
-    max: 255
+    max: 128
   },
   description: {
     type: String,
@@ -16,4 +19,17 @@ const jargonSchema = {
 
 const Jargon = mongoose.model('Jargon', new mongoose.Schema(jargonSchema));
 
-module.exports = Jargon;
+function validateJargon(jargonTerm) {
+  const validationSchema = {
+    type: 'object',
+    properties: {
+      name: { type: 'string', minLength: 2, maxLength: 128 },
+      description: { type: 'string' }
+    }
+  };
+  const validate = ajv.compile(validationSchema); // made asynchronous with 'ajv-async'
+  return validate(jargonTerm); // returns a promise
+}
+
+exports.Jargon = Jargon;
+exports.validate = validateJargon;
