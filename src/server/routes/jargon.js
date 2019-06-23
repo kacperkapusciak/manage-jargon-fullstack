7783478d0dc4d73fa11eb28099866740592fceff
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-06-23 14:06:23 +0200

    Added 'post' jargon route

--- src/server/routes/jargon.js
+++ src/server/routes/jargon.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const Jargon = require('../models/jargon');
+const { Jargon, validate } = require('../models/jargon');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   const jargonTerms = await Jargon.find().sort('name');
-  res.send(jargonTerms);
+  return res.send(jargonTerms);
+});
+
+router.post('/', async (req, res) => {
+  const valid = await validate(req.body);
+  if (!valid) return res.status(400).send('Invalid name or description.');
+
+  const jargonTerm = new Jargon({
+    name: req.body.name,
+    description: req.body.description
+  });
+  await jargonTerm.save();
+  return res.send(jargonTerm);
 });
 
 module.exports = router;
