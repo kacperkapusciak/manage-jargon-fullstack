147b5bb817b0d1c7e2201ff43fb13b7da775a789
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-06-24 22:07:52 +0200

    Added user model and validation

---
+++ src/server/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Ajv = require('ajv');
+
+const ajv = new Ajv();
+require('ajv-async')(ajv);
+
+const userSchema = {
+  username: {
+    type: String,
+    required: true,
+    min: 4,
+    max: 128
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 4,
+    max: 1024 // extra character space for encryption
+  },
+  admin: {
+    type: Boolean
+  }
+};
+
+const User = mongoose.model('User', new mongoose.Schema(userSchema));
+
+function validateUser(user) {
+  const validationSchema = {
+    type: 'object',
+    properties: {
+      username: { type: 'string', minLength: 4, maxLength: 128 },
+      password: { type: 'string', minLength: 4, maxLength: 255 }
+    }
+  };
+  const validate = ajv.compile(validationSchema);
+  return validate(user); // returns a promise
+}
+
+exports.User = User;
+exports.validate = validateUser;
