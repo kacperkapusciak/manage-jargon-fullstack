3f3ec928e916f5dbe951127afff786ffd89d0f5c
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-06-25 13:42:37 +0200

    Logging in the users

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const jargon = require('./routes/jargon');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 mongoose
   .connect('mongodb://localhost/manage-jargon', {
@@ -20,6 +21,7 @@ app.use(express.json());
 
 app.use('/api/jargon/', jargon);
 app.use('/api/users/', users);
+app.use('/api/auth/', auth);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}!`));
