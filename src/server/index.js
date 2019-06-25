c25a7d3a6a3adaf20ca7c438d8b1965eef5acad6
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-06-25 13:27:04 +0200

    Added users route to index.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const jargon = require('./routes/jargon');
+const users = require('./routes/users');
 
 mongoose
   .connect('mongodb://localhost/manage-jargon', {
@@ -18,6 +19,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/jargon/', jargon);
+app.use('/api/users/', users);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}!`));
